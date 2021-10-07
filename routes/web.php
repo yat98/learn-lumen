91ce65a792738a4c59f2dfd3f2a1f65e87f46b5f
yat98 <hidayatchandra08@gmail.com>
2021-10-07 08:23:58 +0800

    Add http request in lumen

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,11 @@ $router->get('/', function () use ($router) {
 $router->get('key', function () {
 	return Str::random(32);
 });
+
+$router->get('foo', function () {
+	return 'Hello, GET method';
+});
+
+$router->post('bar',function(){
+    return 'Hello, POST method';
+});
